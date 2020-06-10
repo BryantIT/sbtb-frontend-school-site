5a0fc2b6ae0904f16e4bffba3a9565ef23ce878c
Bryant Richards <dragonseyeimages@gmail.com>
2020-06-10 12:27:12 -0500

    changed logo in nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Nav from './components/nav/Nav'
 
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Nav />
         <h1>This is App.js</h1>
       </div>
     );
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <header id="header">
+		<div class="container">
+			<a href="index.html" id="logo" title="HarrisonHighSchool">HarrisonHighSchool</a>
+			<div class="menu-trigger"></div>
+			<nav id="menu">
+				<ul>
+					<li><a href="events.html">Courses</a></li>
+					<li><a href="gallery.html">Students</a></li>
+					<li><a href="events.html">Events</a></li>
+ 				</ul>
+				<ul>
+					<li><a href="gallery.html">Teachers</a></li>
+					<li><a href="gallery.html">Gallery</a></li>
+					<li><a href="#fancy" class="get-contact">Contact</a></li>
+   				</ul>
+			</nav>
+		</div>
+	</header>
+  )
+}
+
+export default Nav
