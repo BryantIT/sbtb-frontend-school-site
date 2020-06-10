63e2074fce8788bdb75a972afd97311c42418d87
Bryant Richards <dragonseyeimages@gmail.com>
2020-06-10 13:08:17 -0500

    set up basic home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/nav/Nav'
+import Home from './components/home/Home'
 import { Route, Switch, withRouter  } from 'react-router-dom';
 
 
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div>
         <Nav />
+        <Home />
         <h1>This is App.js</h1>
       </div>
     );
---
+++ src/components/home/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div class="slider">
+		<ul class="bxslider">
+			<li>
+				<div class="container">
+					<div class="info">
+						<h2>It’s Time to <br /><span>Get back to school</span></h2>
+						<a href="#">Check out our new programs</a>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="container">
+					<div class="info">
+						<h2>It’s Time to <br /><span>Get back to school</span></h2>
+						<a href="#">Check out our new programs</a>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="container">
+					<div class="info">
+						<h2>It’s Time to <br /><span>Get back to school</span></h2>
+						<a href="#">Check out our new programs</a>
+					</div>
+				</div>
+			</li>
+		</ul>
+		<div class="bg-bottom"></div>
+	</div>
+  )
+}
+
+export default Home
