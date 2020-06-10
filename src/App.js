a6aa48c858346ed5dd522c053f7a6c1a60e18255
Bryant Richards <dragonseyeimages@gmail.com>
2020-06-10 09:50:54 -0500

    changed default port

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Main App Page</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
