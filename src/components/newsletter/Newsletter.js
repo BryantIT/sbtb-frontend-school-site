a4cf1c27ceccf9308876121b49ab7aef5d8f4f7a
Bryant Richards <dragonseyeimages@gmail.com>
2020-06-12 09:27:24 -0500

    fixed newlsetter styling

--- src/components/newsletter/Newsletter.js
+++ src/components/newsletter/Newsletter.js
@@ -8,9 +8,9 @@ const Newsletter = () => {
 			<article>
 				<div className="pic"><img src="images/1.png" alt="" /></div>
 				<div className="info">
-					<h4>Omnis iste natus error sit voluptatem accusantium </h4>
+					<h4>Newsletter Title </h4>
 					<p className="date">14 APR 2014, Jason Bang</p>
-					<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores (...)</p>
+					<p>Blah Blah Blah, newsletter body, blah blah blah</p>
 					<a className="more" href="#">Read more</a>
 				</div>
 			</article>
