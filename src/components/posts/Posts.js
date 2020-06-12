aa1f9300a52f865a3ee52b2fe7115a87d1662723
Bryant Richards <dragonseyeimages@gmail.com>
2020-06-12 09:15:29 -0500

    fixed class names

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Posts = () => {
   return (
     <div>
-      <section class="posts">
-		<div class="container">
+      <section className="posts">
+		<div className="container">
 			<article>
-				<div class="pic"><img width="121" src="images/2.png" alt="" /></div>
-				<div class="info">
+				<div className="pic"><img width="121" src="images/2.png" alt="" /></div>
+				<div className="info">
 					<h3>Located to inspire</h3>
 					<p>Nestled in sunny, beautiful Pacific Palisades, California students
               enjoy fresh ocean air and amazing scenery. Just minutes away from
@@ -15,8 +15,8 @@ const Posts = () => {
 				</div>
 			</article>
 			<article>
-				<div class="pic"><img width="121" src="images/3.png" alt="" /></div>
-				<div class="info">
+				<div className="pic"><img width="121" src="images/3.png" alt="" /></div>
+				<div className="info">
 					<h3>Programs for all of our students</h3>
 					<p>With extracuricular programs before and after school covering a
               wide range of interests students will be able to find something of
