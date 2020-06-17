ee3d551867e4b787251e461fcc8e5141c696119e
Bryant Richards <dragonseyeimages@gmail.com>
2020-06-17 12:13:15 -0500

    login fetch

--- src/actions/users/currentUser.js
+++ src/actions/users/currentUser.js
@@ -4,3 +4,26 @@ export const setCurrentUser = user => {
     user
   }
 }
+
+export const login = (credentials, history) => {
+  return dispatch => {
+    return fetch("http://localhost:3000/api/v1/login", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(credentials)
+    })
+    .then(r => r.json())
+    .then(response => {
+      if (response.error) {
+        alert(response.error)
+      } else {
+        
+        history.push('/')
+      }
+    })
+    .catch(console.log)
+  }
+}
