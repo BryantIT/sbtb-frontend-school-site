11a0c9f5d5c58d8383a02174a45ff9a5911cb10b
Bryant Richards <dragonseyeimages@gmail.com>
2020-07-15 11:18:43 -0500

    switched from big calendar to rc calendar

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
+import { Calendar, momentLocalizer, Views } from 'react-big-calendar';
 import moment from 'moment';
 
 const localizer = momentLocalizer(moment)
+
 
 const Events = ({ events }) => {
 
@@ -11,8 +12,13 @@ const Events = ({ events }) => {
   return (
     events ?
     <div>
-      <h1>hello</h1>
-      <p>{events.length}</p>
+      <Calendar
+      localizer={localizer}
+      events={events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
 
     </div> : ""
 
