81fa336652e4eaa1c18d4146d02b199b9ea99314
Bryant Richards <dragonseyeimages@gmail.com>
2020-07-16 10:23:39 -0500

    fixed mobile view of calendar

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -5,15 +5,15 @@ class Events extends Component {
 
   render(){
     return (
-      <div className="responsiveCal">
+      <div>
         <Iframe url="https://calendar.google.com/calendar/embed?src=uvcn0gpnkm9s9d3ogbmk9tqv3c%40group.calendar.google.com&ctz=America%2FChicago"
         src='https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23D50000&amp;ctz=America%2FChicago&amp;src=dXZjbjBncG5rbTlzOWQzb2dibWs5dHF2M2NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%239E69AF&amp;color=%23009688&amp;title=Bayside%20High%20Events'
         width="100%"
         height="800px"
         id="myId"
-        className="myClassname"
+        className="responsiveCal"
         display="initial"
-        position="relative"/>
+        position="absolute"/>
       </div>
     )
   }
