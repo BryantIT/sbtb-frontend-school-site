faff39ddefc6a2aa5de0ff874ec9460b0b66a20b
Bryant Richards <dragonseyeimages@gmail.com>
2020-07-16 10:19:24 -0500

    Set up calendar

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -1,30 +1,22 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import Calendar from 'rc-calendar';
-import moment from 'moment';
+import Iframe from 'react-iframe';
 
+class Events extends Component {
 
-const format = 'YYYY-MM-DD';
-const now = moment();
-
-const Events = ({ events }) => {
-
-
-  return (
-    events ?
-    <div>
-      <Calendar />
-
-    </div> : ""
-
-  )
-
-}
-
-const mapStateToProps = ({ events }) => {
-  return {
-    events
+  render(){
+    return (
+      <div className="responsiveCal">
+        <Iframe url="https://calendar.google.com/calendar/embed?src=uvcn0gpnkm9s9d3ogbmk9tqv3c%40group.calendar.google.com&ctz=America%2FChicago"
+        src='https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23D50000&amp;ctz=America%2FChicago&amp;src=dXZjbjBncG5rbTlzOWQzb2dibWs5dHF2M2NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%239E69AF&amp;color=%23009688&amp;title=Bayside%20High%20Events'
+        width="100%"
+        height="800px"
+        id="myId"
+        className="myClassname"
+        display="initial"
+        position="relative"/>
+      </div>
+    )
   }
 }
 
-export default connect(mapStateToProps)(Events)
+export default Events
