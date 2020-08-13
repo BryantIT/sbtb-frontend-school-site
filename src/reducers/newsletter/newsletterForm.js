f6471a19a8cd249d0947db5fd875c98607a20dea
Bryant Richards <dragonseyeimages@gmail.com>
2020-08-13 11:24:33 -0500

    set up newsletter form reducer

---
+++ src/reducers/newsletter/newsletterForm.js
@@ -0,0 +1,24 @@
+const initialState = {
+  title: "",
+  author: "",
+  published: "",
+  body: "",
+  imageUrl: ""
+}
+
+export default (state=initialState, action) => {
+  switch (action.type) {
+    case "UPDATE_NEW_NEWSLETTER_FORM":
+      const returnValue = {
+        ...state,
+        [action.formData.name]: action.formData.value
+      }
+      return returnValue
+    case "RESET_NEW_NEWSLETTER_FORM":
+      return initialState
+    case "SET_NEWSLETTER_DATA_FOR_EDIT":
+      return action.newsletterFormData
+    default:
+      return state
+  }
+}
